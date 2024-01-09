d8fc0400ec23517cd19f7f74e72cbd555e7912c6
psu62 <psu62@LAPTOP-RHB7AIVC>
2024-01-09 18:54:25 +0900

    not yet automatics

--- src/newcalendar/AboutCalendar.java
+++ src/newcalendar/AboutCalendar.java
@@ -9,13 +9,28 @@ public class AboutCalendar {
 	}
 	
 	
+	//시작 요일 구하기 -> 그레고리력
+	public int getWeekDay(int year, int month, int day) {
+		
+		
+		
+		return 0;
+	}
+	
+	
 	
 	public void printCalendar(int year, int month) {
 
 		if((year % 4 == 0) && ((year % 100 != 0) || (year % 400 != 0))) {
 			MAXDAYS[1] = 29;
 		}
-			
+		
+		//get WeekDay automatically
+		int weekday = getWeekDay(year, month, 1);
+		
+		
+		
+		//printout
 		System.out.printf(" << %4d년 %3d월 >>",year, month);
 		System.out.println();
 		System.out.println(" 일 월 화 수 목 금 토 ");
@@ -23,12 +38,32 @@ public class AboutCalendar {
 		
 		int maxOfDays = getMaxOfMonth(month);
 		
-		for(int i = 1; i <= maxOfDays; i++) {
+		// print blank line
+		for (int j = 0; j < weekday; j++) {
+			System.out.printf("   ");
+		}
+		
+		// print first line
+		int count = 7 - weekday;
+		int delim = count;
+		
+		// 수 3 카ㅣ운트 4
+		for(int i = 1; i <= count; i++) {
 			System.out.printf("%3d",i);
-			if(i % 7 == 0) {
+		}
+		System.out.println();
+
+		count++;
+		// print remain line
+		for(int i = count; i <= maxOfDays; i++) {
+			System.out.printf("%3d",i);
+			
+			if(i % 7 == delim) {
 				System.out.println();
 			}
 		}
+		
+		System.out.println();
 		System.out.println();
 		MAXDAYS[1] = 28;
 	}
--- src/newcalendar/AboutPrompt.java
+++ src/newcalendar/AboutPrompt.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class AboutPrompt {
 		private final static String PROMPT = "Cal> ";
 		
+//		public int startDay(String week) {
+//			if(week.equals("일")) return 0;
+//			else if(week.equals("월")) return 1;
+//			else if(week.equals("화")) return 2;
+//			else if(week.equals("수")) return 3;
+//			else if(week.equals("목")) return 4;
+//			else if(week.equals("금")) return 5;
+//			else if(week.equals("토")) return 6;
+//			else return 0;
+//		}
+		
 		public void runPrompt() {
 			Scanner scanner = new Scanner(System.in);
 			AboutCalendar cal1 = new AboutCalendar();
@@ -14,7 +25,7 @@ public class AboutPrompt {
 			
 			while(true) {
 				
-			System.out.println("년도를 입력하시고 엔터하세요(종료 원할시 : -1)");
+			System.out.println("년도를 입력하시고 엔터하세요");
 			System.out.print(PROMPT);
 			year = scanner.nextInt();
 					
@@ -23,6 +34,10 @@ public class AboutPrompt {
 			System.out.print(PROMPT);
 			month = scanner.nextInt();
 			
+//			System.out.println("첫번째 요일을 입력하세요!");
+//			System.out.print(PROMPT);
+//			String firstDay = scanner.next();
+//			int week = startDay(firstDay);
 			
 			if(month > 12) {
 				System.out.println("옳지 않습니다, 다른 숫자를 입력하세요.");
